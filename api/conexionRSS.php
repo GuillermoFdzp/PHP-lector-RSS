dfd0d5ea493bd4799089f24ec82e33a70a87fc68
GuillermoFdzp <guillermo.fernandez.perez2005@gmail.com>
2025-02-03 10:31:06 +0100

    volvemos

--- api/conexionRSS.php
+++ api/conexionRSS.php
@@ -1,6 +1,7 @@
 <?php
 
 function download($ruta){
+    $ch=curl_init();
     curl_setopt($ch,CURLOPT_URL,$ruta);
     curl_setopt($ch,CURLOPT_POST,0); // post descarga datos y get los sube
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
