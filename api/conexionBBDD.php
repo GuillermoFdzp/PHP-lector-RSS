fcce16f0157c192b114b47de0db333822c16efdb
GuillermoFdzp <guillermo.fernandez.perez2005@gmail.com>
2025-02-03 10:28:13 +0100

    cambios

--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -1,10 +1,12 @@
 <?php
 
 $Repit=false;
-$host="postgres://neondb_owner:npg_sK5GNmXbpA3Z@ep-frosty-surf-a2msft3e-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require";
-$user="neondb_owner";
-$password="npg_sK5GNmXbpA3Z";
+$conn_string="postgres://neondb_owner:npg_sK5GNmXbpA3Z@ep-frosty-surf-a2msft3e-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require";
+$link = pg_connect($conn_string);
 
-$link= mysqli_connect($host,$user,$password);
-$tildes=$link->query("SET NAMES 'utf8'");
-mysqli_select_db($link,'neondb');
+if (!$link) {
+    die("Error in connection: " . pg_last_error());
+}
+
+pg_set_client_encoding($link, "UTF8");
+echo "Conectado a la base de datos";
