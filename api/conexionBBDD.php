f3caf38df034dd60bc7639496ef7484a08beb0cf
GuillermoFdzp <guillermo.fernandez.perez2005@gmail.com>
2025-02-03 10:12:35 +0100

    base datos

--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -1,10 +1,10 @@
 <?php
 
 $Repit=false;
-$host="localhost";
-$user="root";
-$password="";
+$host="postgres://neondb_owner:npg_sK5GNmXbpA3Z@ep-frosty-surf-a2msft3e-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require";
+$user="neondb_owner";
+$password="npg_sK5GNmXbpA3Z";
 
 $link= mysqli_connect($host,$user,$password);
 $tildes=$link->query("SET NAMES 'utf8'");
-mysqli_select_db($link,'periodicos');
+mysqli_select_db($link,'neondb');
