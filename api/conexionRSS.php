64c49492e65db5fdda52fe468fa44d990f29c4e3
GuillermoFdzp <guillermo.fernandez.perez2005@gmail.com>
2025-02-04 14:03:02 +0100

    trying

--- api/conexionRSS.php
+++ api/conexionRSS.php
@@ -5,7 +5,7 @@ function download($ruta){
     curl_setopt($ch,CURLOPT_URL,$ruta);
     curl_setopt($ch,CURLOPT_POST,0); // post descarga datos y get los sube
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-    //curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1); // sirve para alojarlo en una libreria concreta
+    curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1); // sirve para alojarlo en una libreria concreta
     curl_setopt($ch,CURLOPT_HEADER,false);
     $salida= curl_exec($ch);
     curl_close($ch);
